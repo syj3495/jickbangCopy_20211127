cba339d25010d7827a5a31508e82341c547da79b
yeonjin seong <syj3405@naver.com>
2021-11-27 12:18:56 +0900

    어댑터 클래스 뻐대작업 마루리

--- app/src/main/java/com/neppplus/jickbangcopy_20211127/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211127/adapters/RoomAdapter.kt
@@ -1,12 +1,36 @@
 package com.neppplus.jickbangcopy_20211127.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20211127.R
 import com.neppplus.jickbangcopy_20211127.datas.RoomData
 
 class RoomAdapter(
-    val mcontext : Context,
+    val mContext : Context,
     val resId : Int,
     val mList: ArrayList<RoomData>
-): ArrayAdapter<RoomData>(mcontext, resId, mList) {
+): ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if( tempRow == null){
+
+            tempRow = mInflater.inflate( R.layout.room_list_item, null )
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
 }
+
